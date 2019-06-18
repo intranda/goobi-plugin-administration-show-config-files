7f7f7636ee888894278a15a2ab642ae88190b0d5
Oliver Paetzel <oliver.paetzel@intranda.com>
2019-06-18 13:12:59 +0200

    fix bugs - works as expected now

--- goobi-plugin-administration-show-configfiles/src/main/java/de/intranda/goobi/plugins/ShowConfigFilesPlugin.java
+++ goobi-plugin-administration-show-configfiles/src/main/java/de/intranda/goobi/plugins/ShowConfigFilesPlugin.java
@@ -2,9 +2,12 @@ package de.intranda.goobi.plugins;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -12,10 +15,19 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.apache.commons.configuration.XMLConfiguration;
+import org.apache.commons.configuration.tree.xpath.XPathExpressionEngine;
 import org.goobi.production.enums.PluginType;
 import org.goobi.production.plugin.interfaces.IAdministrationPlugin;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ListObjectsRequest;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+
+import de.sub.goobi.config.ConfigPlugins;
 import de.sub.goobi.config.ConfigurationHelper;
+import de.sub.goobi.helper.S3FileUtils;
 import de.sub.goobi.helper.StorageProvider;
 import lombok.Getter;
 import lombok.extern.log4j.Log4j;
@@ -24,16 +36,25 @@ import net.xeoh.plugins.base.annotations.PluginImplementation;
 @PluginImplementation
 @Log4j
 public class ShowConfigFilesPlugin implements IAdministrationPlugin {
+    private final static String TITLE = "intranda_administration_showconfigfiles";
     private List<String> configFiles = new ArrayList<>();
     private Map<String, String> contents = new HashMap<>();
+    private XMLConfiguration xmlConf;
     @Getter
     private String currentConfig;
     @Getter
+    private String currentConfigContent = " - select a file from the list on the left - ";
+    @Getter
     private String configClass;
+
+    public ShowConfigFilesPlugin() {
+        xmlConf = ConfigPlugins.getPluginConfig(TITLE);
+        xmlConf.setExpressionEngine(new XPathExpressionEngine());
+    }
 
     @Override
     public String getTitle() {
-        return "intranda_administration_showconfigfiles";
+        return TITLE;
     }
 
     @Override
@@ -55,6 +76,7 @@ public class ShowConfigFilesPlugin implements IAdministrationPlugin {
     }
 
     public void loadConfig(String file) {
+        this.currentConfig = file;
         if (!contents.containsKey(file)) {
             ConfigurationHelper config = ConfigurationHelper.getInstance();
             Path p = Paths.get(config.getConfigurationFolder(), file);
@@ -72,7 +94,44 @@ public class ShowConfigFilesPlugin implements IAdministrationPlugin {
         } else {
             this.configClass = "";
         }
-        this.currentConfig = contents.get(file);
+        this.currentConfigContent = contents.get(file);
+    }
+
+    public boolean isRenderUpdateButton() {
+        return xmlConf.getBoolean("/s3Update/enabled", false);
+    }
+
+    public void downloadConfigFromS3() {
+        AmazonS3 s3 = S3FileUtils.createS3Client();
+        Path dest = Paths.get(ConfigurationHelper.getInstance().getConfigurationFolder());
+        String bucket = xmlConf.getString("/s3Update/bucket");
+        String folderPrefix = xmlConf.getString("/s3Update/prefix");
+        ListObjectsRequest req = new ListObjectsRequest().withBucketName(bucket).withPrefix(folderPrefix);
+        ObjectListing listing = s3.listObjects(req);
+        for (S3ObjectSummary os : listing.getObjectSummaries()) {
+            downloadObject(s3, dest, os);
+        }
+        while (listing.isTruncated()) {
+            listing = s3.listNextBatchOfObjects(listing);
+            for (S3ObjectSummary os : listing.getObjectSummaries()) {
+                downloadObject(s3, dest, os);
+            }
+        }
+        this.contents = new HashMap<>();
+        this.loadConfig(this.currentConfig);
+    }
+
+    public void downloadObject(AmazonS3 s3, Path dest, S3ObjectSummary os) {
+        String filename = os.getKey();
+        int idx = filename.lastIndexOf('/');
+        if (idx > 0) {
+            filename = (filename.substring(idx + 1));
+        }
+        try (InputStream in = s3.getObject(os.getBucketName(), os.getKey()).getObjectContent()) {
+            Files.copy(in, dest.resolve(filename), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            log.error(e);
+        }
     }
 
 }
